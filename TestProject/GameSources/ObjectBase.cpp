b318723b2ccdf97064dc5e7a7e899d6e7992f1de
達哉 阿部 <zeal18300092@c-league.jp>
2020-03-30 09:57:17 +0900

    [new]テストプロジェクトにUtility追加・汎用入力受付追加

---
+++ TestProject/GameSources/ObjectBase.cpp
@@ -0,0 +1,11 @@
+/*!
+@breif	オブジェクトの基底クラス実体
+@name	作成者：阿部達哉
+*/
+#include "stdafx.h"
+#include "Project.h"
+
+namespace basecross
+{
+
+}
---
+++ TestProject/GameSources/ObjectBase.h
@@ -0,0 +1,45 @@
+/*!
+@breif	オブジェクトの基底クラス
+@name	作成者：阿部達哉
+*/
+#pragma once
+#include "stdafx.h"
+#include "ProjectUtility.h"
+
+namespace basecross
+{
+	//---------------------------------------------------
+	//配置オブジェクトの基底クラス
+	//---------------------------------------------------
+	class ObjectBase :public GameObject
+	{
+	public:
+		ObjectBase(const shared_ptr<Stage>&StagePtr)
+			:GameObject(StagePtr) {}
+		virtual ~ObjectBase() {}
+
+		//初期化
+		virtual void OnCreate()override = 0;
+		//更新処理
+		virtual void OnUpdate()override = 0;
+	};
+
+	//---------------------------------------------------
+	//入力操作可能クラスの基底クラス
+	//---------------------------------------------------
+	template <typename T>
+	class PawnBase
+	{
+	public:
+		PawnBase(){}
+
+		virtual ~PawnBase() {}
+
+		virtual void OnPushA() {};
+		virtual void OnPushB() {};
+		virtual void OnPushX() {};
+		virtual void OnPushY() {};
+	protected:
+		InputHandler<T> m_Handler;
+	};
+}
