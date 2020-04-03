29804e5a8f558f46f60ad5cb49a79e7ebede1057
KoukiMatsuzaki <zeal18300086@c-league.jp>
2020-04-03 11:35:45 +0900

    [new]enumクラス追加、プレイヤースタート時・プレイヤーゴール時追加

--- TestProject/GameSources/Player.cpp
+++ TestProject/GameSources/Player.cpp
@@ -8,14 +8,18 @@
 #include "Player.h"
 
 namespace basecross{
+
+	//---------------------------------------------------
+	//プレイヤークラスの実体
+	//---------------------------------------------------
 	Player::Player(const shared_ptr<Stage>& StagePtr) :
 		ObjectBase(StagePtr),
 		m_cntlNum(0),
 		m_Speed(3.0f),
 		m_Jumpforce(4.0f),
-		m_Dethtime(0.0f),
+		m_StopActionTime(5.0f),
 		m_Jumpjudge(false),
-		m_Dethtimejudge(false)
+		m_StopActionTimeJudge(false)
 	{}
 	
 
@@ -42,7 +46,9 @@ namespace basecross{
 		Shadowptr->SetMeshResource(L"DEFAULT_CUBE");
 		auto ptrDraw = AddComponent<BcPNTStaticDraw>();
 		ptrDraw->SetMeshResource(L"DEFAULT_CUBE");
+		m_PlayerState = PlayerState::Start;
 	}
+
 
 	//進行ベクトルへの下方関数
 	Vec3 Player::GetMoveVector() const{
@@ -81,40 +87,58 @@ namespace basecross{
 	void Player::Move() {
 		auto elapsedtime = App::GetApp()->GetElapsedTime();
 		m_PlayerAngle = GetMoveVector();
-		if (m_PlayerAngle.length() > 0.0f && !m_Dethtimejudge) {
+		if (m_PlayerAngle.length() > 0.0f/* && !m_StopActionTimeJudge*/) {
 			auto pos = GetComponent<Transform>()->GetPosition();
 			pos += m_PlayerAngle * elapsedtime * m_Speed;
 			GetComponent<Transform>()->SetPosition(pos);
 		}
-		if (m_PlayerAngle.length() > 0.0f && !m_Dethtimejudge) {
+		if (m_PlayerAngle.length() > 0.0f/* && !m_StopActionTimeJudge*/) {
 			auto utilPtr = GetBehavior<UtilBehavior>();
 			utilPtr->RotToHead(m_PlayerAngle, 1.0f);
 		}
-		if (m_Dethtimejudge) {
-			m_Dethtime += elapsedtime;
+	}
+
+	void Player::StartState() {
+		auto elapsedtime = App::GetApp()->GetElapsedTime();
+		if (m_StopActionTimeJudge) {
+			m_StopActionTime -= elapsedtime;
 		}
-		if (m_Dethtime >= 3.0f) {
-			m_Dethtimejudge = false;
-			m_Dethtime = 0.0f;
+		if (m_StopActionTime <= 0.0f) {
+			m_StopActionTimeJudge = false;
+			m_PlayerState = PlayerState::Excute;
+			m_StopActionTime = 5.0f;
+		}
+
+	}
+
+	void Player::ClearState() {
+		auto elapsedtime = App::GetApp()->GetElapsedTime();
+		if (m_StopActionTimeJudge) {
+			m_StopActionTime -= elapsedtime;
+		}
+		if (m_StopActionTime <= 0.0f) {
+			m_StopActionTime = 5.0f;
 		}
 	}
 
 	void Player::OnPushA() {
-		if (/*m_cntl.A && */m_Jumpjudge) {
+		if (m_Jumpjudge && !m_StopActionTimeJudge) {
 			auto grav = GetComponent<Gravity>();
 			grav->StartJump(Vec3(0.0f, m_Jumpforce, 0.0f));
 			m_Jumpjudge = false;
 		}
-		//m_cntl.A = false;
 
 	}
-
 	void Player::OnCollisionEnter(shared_ptr<GameObject>& Obj) {
 		auto ptrTransform = GetComponent<Transform>();
 		if (Obj->FindTag(L"Deth")) {
 			ptrTransform->SetPosition(0.0f, 0.5f, 0.0f);
 			ptrTransform->SetRotation(0.0f, 0.0f, 0.0f);
-			m_Dethtimejudge = true;
+			m_StopActionTimeJudge = true;
+			m_PlayerState = PlayerState::Start;
+		}
+		if (Obj->FindTag(L"Goal")) {
+			m_PlayerState = PlayerState::Clear;
 		}
 	}
 
@@ -127,7 +151,28 @@ namespace basecross{
 	void Player::OnUpdate() {
 		m_inputHandler.PushHandle(m_cntlNum, m_cntl);
 		m_Handler.PushHandler(GetThis<Player>());
-		Move();
+		State();
+	}
+
+	void Player::State() {
+		switch (m_PlayerState){
+		case PlayerState::Start: 
+		{
+			m_StopActionTimeJudge = true;
+			StartState();
+		}
+			break;
+		case PlayerState::Excute:
+		{
+			Move();
+		}
+		break;
+		case PlayerState::Clear:
+		{
+			ClearState();
+		}
+		break;
+		}
 	}
 
 
