104c273a8e0f9eaba6ff16374f788e06776667de
達哉 阿部 <zeal18300092@c-league.jp>
2020-04-22 13:29:29 +0900

    [update][main]現在の進行度一時保存・Playerの当たり判定修正

--- MainGameProject/GameSources/Scene.cpp
+++ MainGameProject/GameSources/Scene.cpp
@@ -18,6 +18,7 @@ namespace basecross{
 			Col4 Col;
 			Col.set(31.0f / 255.0f, 30.0f / 255.0f, 71.0f / 255.0f, 255.0f / 255.0f);
 			SetClearColor(Col);
+			_EfkInterface = ObjectFactory::Create<EfkInterface>();
 			SetDebugResoruce();
 			GameManager::CreateManager();
 			//自分自身にイベントを送る
@@ -55,6 +56,55 @@ namespace basecross{
 		}
 	}
 
+	void Scene::SetResoruceData(const wstring ResourceFile)
+	{
+		wstring PathStr;
+		App::GetApp()->GetDataDirectory(PathStr);
+		XmlDocReader ResFile(PathStr+ResourceFile);
+		auto ResPtr = ResFile.GetSelectNodes(L"Resource");
+		
+		long count = XmlDocReader::GetLength(ResPtr);
+		for (long index = 0; count < index; index++)
+		{
+			auto ResItem = XmlDocReader::GetItem(ResPtr, index);
+			auto TypeStr = XmlDocReader::GetAttribute(ResItem, L"ResourceType");
+			auto KeyStr = XmlDocReader::GetAttribute(ResItem, L"ResourceKey");
+			auto FileStr = XmlDocReader::GetAttribute(ResItem, L"ResourceFile");
+
+			if (TypeStr == L"Texture")
+			{
+				auto TexPath = PathStr + L"Texture/";
+				App::GetApp()->RegisterTexture(KeyStr, TexPath + FileStr);
+				continue;
+			}
+			else if (TypeStr == L"Model")
+			{
+				auto MDPath = PathStr + L"Model/";
+				auto modelMesh = MeshResource::CreateStaticModelMesh(MDPath, L"MatTest.bmf");
+				App::GetApp()->RegisterResource(KeyStr, modelMesh);
+				continue;
+			}
+			else if (TypeStr == L"Sound")
+			{
+				auto SDPath = PathStr + L"Sound/";
+				App::GetApp()->RegisterWav(KeyStr, SDPath + FileStr);
+				continue;
+			}
+			else if (TypeStr == L"Effect")
+			{
+				auto EFKPath = PathStr + L"Effect/";
+				auto MagniStr = XmlDocReader::GetAttribute(ResItem, L"EffectMagni");
+				float Magni = 1.0f;
+				if (MagniStr != L"")
+				{
+					Magni = (float)_wtof(MagniStr.c_str());
+				}
+				App::GetApp()->RegisterEffect(KeyStr, EFKPath + FileStr, _EfkInterface, Magni);
+				continue;
+			}
+		}
+	}
+
 	void Scene::SetDebugResoruce()
 	{
 		wstring MediaPath;
@@ -67,8 +117,10 @@ namespace basecross{
 		App::GetApp()->RegisterTexture(L"GameStage_TX", MediaPath + L"GameStage.png");
 		App::GetApp()->RegisterTexture(L"EndingStage_TX", MediaPath + L"EndingStage.png");
 
-		App::GetApp()->RegisterTexture(L"TEST_TX", MediaPath + L"itimatu.bmp");
-
+		//MDTex
+		//App::GetApp()->RegisterTexture(L"TEST_TX", MediaPath + L"itimatu.bmp");
+		App::GetApp()->RegisterTexture(L"TEST_TX", MediaPath + L"kabe.jpg");
+		App::GetApp()->RegisterTexture(L"FLOOR_TX", MediaPath + L"ytail.jpg");
 	}
 
 }
